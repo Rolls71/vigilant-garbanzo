c6730b24e9afa2ba0d79909a09ecb88ddb96aa06
Josh Edwards <josh@terasoft.com.au>
2024-03-17 21:00:22 +1100

    Add draggable image

--- app.js
+++ app.js
@@ -54,3 +54,17 @@ function renderMap() {
         this.innerHTML = ""
     })
 }
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
